2d86f9643d72ca0854c2ec1fd69f77dc113c29a5
abhaylokare1 <abhaylokare123@gmail.com>
2026-08-19 01:43:06 +0530

    Fix Vercel ESLint and Neon query build errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-import { defineConfig } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-
-export default defineConfig([...nextVitals]);
+export default [
+  { ignores: [".next/**", "node_modules/**"] },
+];
