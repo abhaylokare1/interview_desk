978d327f5e95cd5bcea59aa5ade092a4acf22a35
abhaylokare1 <abhaylokare123@gmail.com>
2026-08-19 12:21:36 +0530

    Upgrade Next.js security patch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const baseDirectory = dirname(fileURLToPath(import.meta.url));
+const compat = new FlatCompat({ baseDirectory });
+
 export default [
   { ignores: [".next/**", "node_modules/**"] },
+  ...compat.extends("next/core-web-vitals"),
 ];
